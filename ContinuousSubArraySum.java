3c4756391c730d8c8bf9d6a7164e6e64fb0d35b1
Amit Dubey <adubey31@gmail.com>
2021-04-23 14:00:19 +0530

    Update ContinuousSubArraySum.java

--- ContinuousSubArraySum.java
+++ ContinuousSubArraySum.java
@@ -1,1 +1,33 @@
+/**
+This solution is applicable for positive numbers
 
+Time complexity - O(N)
+Spcace complexity - O(1)
+**/
+public int[] continuousSubArray(int[]  data, int size, int sum){
+  boolean found;
+  int[] result = new int[2];
+  
+  int firstIndex=0;  
+  int s=0;
+  
+  for(int i=0; i<size;i++){
+    s = s + arr[i];
+    
+    if(s > sum){
+      s = s - arr[index++]
+    }
+    
+    if(s == sum){
+      found = true;
+      result[0] = index;
+      result[1] = i;
+      break;
+    }
+  }
+  
+  if(found){
+    return result;
+  }
+  return null;
+}
